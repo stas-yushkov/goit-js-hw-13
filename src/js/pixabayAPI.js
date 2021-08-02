da61db43fa1f151689f205cedbd6c37ce333126f
stas-yushkov <stas.yushkov@gmail.com>
2021-08-02 22:43:03 +0300

    comment

--- src/js/pixabayAPI.js
+++ src/js/pixabayAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
 const key = process.env.PIXABAY_API_KEY;
 console.log('🚀 ~ key', key);
